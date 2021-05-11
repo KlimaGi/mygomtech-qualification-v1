2a9c3063f374ca37a5ae44a99ff94ae7d9d5d0b6
Martynas Baltutis <martynas.baltutis@gmail.com>
2021-05-12 00:10:08 +0300

    set up logout

--- src/components/UsersManagement/components/Header/Header.tsx
+++ src/components/UsersManagement/components/Header/Header.tsx
@@ -1,25 +1,30 @@
-import {FC} from 'react';
-import {IItem} from "~/services/getUserItems";
-import logout from '../../../../services/logout';
+import { FC } from "react";
+import { IItem } from "~/services/getUserItems";
+import logout from "../../../../services/logout";
+import { Routes } from "~/constants";
+import { useHistory } from "react-router-dom";
 
-import './header-style.scss';
+import "./header-style.scss";
 
 interface IHeader {
   items: Array<IItem>;
   username: string;
 }
 
-const Header: FC<IHeader> = ({items, username}) => {
+const Header: FC<IHeader> = ({ items, username }) => {
+  const { push } = useHistory();
 
   return (
     <div className="header">
       <div className="user-section">
-        <button onClick={logout}>{`Logout ${username}`}</button>
+        <button onClick={() => {logout();push(Routes.Login);}}>{`Logout ${username}`}</button>
       </div>
       <h1>{`${items.length} Emails are wrong`}</h1>
-      <span>Email validator to protect your company from bad registrations</span>
+      <span>
+        Email validator to protect your company from bad registrations
+      </span>
     </div>
-  )
+  );
 };
 
 export default Header;
--- src/services/logout.ts
+++ src/services/logout.ts
@@ -1,5 +1,8 @@
 const logout = () => {
-
+  //   const { push } = useHistory();
+  //   push(Routes.Login);
+  localStorage.clear();
+  console.log("hi");
 };
 
 export default logout;
