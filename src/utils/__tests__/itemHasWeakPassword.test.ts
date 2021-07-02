c8ebdd749a02d78f019cd2486a84d8e2fc0214c5
Justas Česnauskas <justas.cesnauskas@mygom.tech>
2021-07-02 10:45:26 +0300

    fix: remove old task tests

--- src/utils/__tests__/itemHasWeakPassword.test.ts
+++
@@ -1,6 +0,0 @@
-import itemHasWeakPassword from '../itemHasWeakPassword';
-import { IItem } from '../../services/getUserItems';
-
-describe('should return true if email do not match requirements', () => {
-
-});
