679bc6668378315cb3619d1cf74d3ae109b839ee
Giedre Klimaite <giedre.klima@gmail.com>
2021-08-17 20:45:27 +0300

    get older  items  than 30d more precisely

--- src/utils/itemIsOlder.ts
+++ src/utils/itemIsOlder.ts
@@ -1,9 +1,9 @@
 import { IItem } from "~/services/getUserItems";
 
 const itemIsOlder = (itemList: Array<IItem>) => {
-  const today = Number(new Date().toISOString().slice(5, 7));
+  const today = new Date().getTime();
   const oldItems = itemList.filter(
-    (item) => today - Number(item.createdAt.slice(5, 7)) >= 1
+    (item) => today - Date.parse(item.createdAt) >= 30 * 24 * 3600 * 1000
   );
   return oldItems;
 };
