5b7d436e19a0fc5b23fd15b3418b7e7191434403
Giedre Klimaite <giedre.klima@gmail.com>
2021-08-18 13:26:16 +0300

    validation username and password

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -12,15 +12,34 @@ const Login = () => {
   const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState<string>();
 
+  const [usernameErr, setUsernameErr] = useState("");
+  const [passwordErr, setPasswordErr] = useState("");
+
   const handleSubmit = async (event: SyntheticEvent<HTMLFormElement>) => {
     event.preventDefault();
     setErrorMessage(null);
-
+    formValidation();
     try {
       await login(username, password);
       push(Routes.Users);
     } catch (error) {
-      setErrorMessage(error.message);
+      setErrorMessage("Your username or password is incorrect");
+    }
+  };
+
+  const formValidation = () => {
+    if (username.length == 0) {
+      setUsernameErr("Your username is required.");
+    } else if (/[!@#$%&*?]/.test(username)) {
+      setUsernameErr("Your username can not have this this speacial character");
+    } else if (username.length <= 3) {
+      setUsernameErr("Your username should have at least 3 letters.");
+    }
+
+    if (password.length == 0) {
+      setPasswordErr("Password is required.");
+    } else if (password.length <= 3) {
+      setPasswordErr("Your password should be longer.");
     }
   };
 
@@ -35,6 +54,7 @@ const Login = () => {
           type="text"
           className="input mt-52px"
         />
+        <ErrorBlock error={usernameErr} />
         <input
           value={password}
           onChange={(event) => setPassword(event.target.value)}
@@ -42,10 +62,12 @@ const Login = () => {
           type="password"
           className="input mt-24px"
         />
-        <ErrorBlock error={errorMessage} />
+        <ErrorBlock error={passwordErr} />
+
         <button type="submit" className="button mt-24px">
           Login
         </button>
+        <ErrorBlock error={errorMessage} />
       </form>
     </div>
   );
