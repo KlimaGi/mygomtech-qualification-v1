aae89d49a6e82fb915f577428ae7bce166337e66
Giedre Klimaite <giedre.klima@gmail.com>
2021-08-23 02:04:12 +0300

    changes for tests

--- src/setupTests.js
+++
@@ -1,7 +0,0 @@
-import Enzyme from "@types/enzyme";
-import Adapter from "enzyme-adapter-react-16";
-
-Enzyme.configure({ adapter: new Adapter() });
-
-import fetch from "@types/node-fetch";
-global.fetch = fetch;
--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,5 +1,22 @@
+/**
+ * @jest-environment jsdom
+ */
+import "jsdom-global";
+import { shallow, mount } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import { MemoryRouter } from "react-router";
 import App from "../App";
+import Login from "../components/Login/Login";
+import PublicRoute from "../components/PublicRoute";
+import PrivateRoute from "../components/PrivateRoute";
 
 describe("<App />", () => {
-  test("should render an App", async () => {});
+  test("should render a Login", () => {
+    const component = shallow(
+      <MemoryRouter>
+        <PublicRoute />
+      </MemoryRouter>
+    );
+    expect(component.find(Login)).toHaveLength(1);
+  });
 });
