0db77633715fabf6a557eec914ce029de66d9e9f
Giedre Klimaite <giedre.klima@gmail.com>
2021-08-31 14:11:28 +0300

    tests for login validation

--- src/__tests__/components/Login.test.tsx
+++ src/__tests__/components/Login.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, cleanup } from "@testing-library/react";
+import { render, screen, cleanup, waitFor } from "@testing-library/react";
 import user from "@testing-library/user-event";
 import Login from "../../components/Login/Login";
 
@@ -25,10 +25,28 @@ describe("Login", () => {
   describe("when the user enter text in input fields", () => {
     it("should show input with the user entered username", () => {
       user.type(screen.getByPlaceholderText(/username/i), "some");
-      expect(screen.getByRole("textbox")).toHaveValue("some");
+      expect(screen.getByRole("textbox")).toHaveAttribute("value", "some");
     });
 
-    it("should show error text, when user leave empty input and click the btn", () => {
+    it("should correctly receive password filed with entered text", () => {
+      user.type(screen.getByPlaceholderText(/password/i), "somepsw");
+      expect(screen.getByPlaceholderText(/password/i)).toHaveAttribute(
+        "value",
+        "somepsw"
+      );
+    });
+  });
+
+  describe("when the user enters input fields incorrectly", () => {
+    afterEach(async () => {
+      await waitFor(() =>
+        expect(
+          screen.getByText(/your username or password is incorrect/i)
+        ).toBeInTheDocument()
+      );
+    });
+
+    it("should show error text, when user leave empty inputs and click the btn", () => {
       user.type(screen.getByPlaceholderText(/username/i), "");
       user.type(screen.getByPlaceholderText(/password/i), "");
 
@@ -37,9 +55,42 @@ describe("Login", () => {
       expect(
         screen.getByText(/your username is required/i)
       ).toBeInTheDocument();
-      expect(screen.getByText(/password is required/i)).toBeInTheDocument();
 
-      // await screen.findByText("Clicked twice");
+      expect(screen.getByText(/password is required/i)).toBeInTheDocument();
+    });
+
+    it("should show error text when the user enters special characters instead letters in username field", () => {
+      user.type(screen.getByPlaceholderText(/username/i), "!@text");
+
+      user.click(screen.getByText(/login/i));
+
+      expect(
+        screen.getByText(/your username can not have this speacial character/i)
+      ).toBeInTheDocument();
+    });
+
+    it("should show error text when the user enters less than 3 letters in username field", () => {
+      user.type(screen.getByPlaceholderText(/username/i), "wu");
+
+      user.click(screen.getByText(/login/i));
+
+      expect(
+        screen.getByText(/your username should have at least 3 letters/i)
+      ).toBeInTheDocument();
+    });
+
+    it("should show error text when the user enters less than 3 symbols in password field", () => {
+      user.type(screen.getByPlaceholderText(/password/i), "ww");
+
+      user.click(screen.getByText(/login/i));
+
+      expect(
+        screen.getByText(/your password should be longer/i)
+      ).toBeInTheDocument();
     });
   });
+
+  it.todo(
+    "should render new component when the user enters correct username and input"
+  );
 });
