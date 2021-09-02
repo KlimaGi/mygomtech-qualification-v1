a444fdc4852b748de5d296d6849c1535f99bfa71
Giedre Klimaite <giedre.klima@gmail.com>
2021-09-02 22:58:35 +0300

    filterTab test

--- src/__tests__/components/FilterTab.test.tsx
+++ src/__tests__/components/FilterTab.test.tsx
@@ -2,10 +2,17 @@ import { fireEvent, render, screen } from "@testing-library/react";
 import FilterTab from "../../components/UsersManagement/components/Filter/components/FilterTab";
 
 describe("FilterTab", () => {
-  fit("should ", () => {
+  beforeEach(() => {
+    render(<FilterTab title="some" count={3} path={"/wrong"} />);
+  });
+  fit("should call right function", () => {
     const onClick = jest.fn();
     const { getByText } = render(<div onClick={onClick}>Wrong(2)</div>);
     fireEvent.click(getByText("Wrong(2)"));
     expect(onClick).toHaveBeenCalledTimes(1);
   });
+
+  it.todo("should show right list content when user clicks the filterTab btn");
+
+  // use async, fetch
 });
